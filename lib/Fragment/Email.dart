aedeb4a460f69df6b2c004b1e39675228671459e
bdrasel <rh88248@gmail.com>
2025-11-23 13:08:01 +0600

    Add new fragments for Settings, SOS, User, Email, Contact, and Home; update main layout to use TabBar

---
+++ lib/Fragment/Email.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Email extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text("Search Email")));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/Fragment/Contact.dart';
+import 'package:my_app/Fragment/Email.dart';
+import 'package:my_app/Fragment/Home.dart';
+import 'package:my_app/Fragment/SearchFragement.dart';
+import 'package:my_app/Fragment/Settings.dart';
+import 'package:my_app/Fragment/Sos.dart';
+import 'package:my_app/Fragment/User.dart';
 
 main() {
   runApp(const MyApp());
@@ -28,360 +35,49 @@ class HomeActivity extends StatelessWidget {
     ).showSnackBar(SnackBar(content: Text(message)));
   }
 
-  alertDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("Alert!"),
-          content: Text("Do you want to delete?"),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text("Cancel"),
-            ),
-            TextButton(
-              onPressed: () {
-                mySnackBar("Delete Success", context);
-                Navigator.pop(context);
-              },
-              child: Text("Delete"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  final List<Map<String, dynamic>> myListItem = [
-    {
-      "name": "Rasel",
-      "age": 24,
-      "roll": 220812,
-      "img":
-          "https://w0.peakpx.com/wallpaper/887/923/HD-wallpaper-garden-waterfalls-garden-imge-nature-waterfalls.jpg",
-    },
-    {
-      "name": "Jony",
-      "age": 26,
-      "roll": 220813,
-      "img":
-          "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000",
-    },
-    {
-      "name": "Sakib",
-      "age": 30,
-      "roll": 220814,
-      "img":
-          "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg",
-    },
-    {
-      "name": "Kalam",
-      "age": 28,
-      "roll": 220815,
-      "img":
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS635r9wzZ_KdmJO2C-1B7HsWkxtbg5b17AUqHUfGxfYt8MJXqrUNrbjYgW_u8v64jcosw&usqp=CAU",
-    },
-    {
-      "name": "Salam",
-      "age": 22,
-      "roll": 220816,
-      "img":
-          "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000",
-    },
-    {
-      "name": "Balam",
-      "age": 27,
-      "roll": 220817,
-      "img":
-          "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg",
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle buttonStyle = TextButton.styleFrom(
-      padding: EdgeInsets.all(10),
-      backgroundColor: Colors.amberAccent,
-      foregroundColor: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-      ),
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Expense Tracker"),
-        backgroundColor: Colors.amberAccent,
-        titleSpacing: 10,
-        toolbarHeight: 60,
-        toolbarOpacity: 0.6,
-        elevation: 40,
-        actions: [
-          IconButton(
-            onPressed: () {
-              mySnackBar('I am Cabin', context);
-            },
-            icon: Icon(Icons.cabin),
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter App"),
+          backgroundColor: Colors.greenAccent,
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.settings), text: 'Settings'),
+              Tab(icon: Icon(Icons.sos), text: 'SOS'),
+              Tab(icon: Icon(Icons.search), text: 'Search'),
+              Tab(icon: Icon(Icons.person), text: 'User'),
+              Tab(icon: Icon(Icons.email), text: 'Email'),
+              Tab(icon: Icon(Icons.contact_emergency), text: 'Contact'),
+            ],
           ),
-          IconButton(
-            onPressed: () {
-              mySnackBar('I am Comment', context);
-            },
-            icon: Icon(Icons.comment),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        elevation: 4,
-        backgroundColor: Colors.amberAccent,
-        child: Icon(Icons.add),
-        onPressed: () {
-          mySnackBar('My Floating Button', context);
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.call), label: 'Call'),
-          BottomNavigationBarItem(icon: Icon(Icons.sos), label: 'SOS'),
-        ],
-        backgroundColor: Colors.amberAccent,
-        onTap: (int index) {
-          if (index == 0) {}
-        },
-      ),
-      drawer: Drawer(
-        child: ListView(
+        ),
+        body: TabBarView(
           children: [
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-              child: UserAccountsDrawerHeader(
-                decoration: BoxDecoration(color: Colors.grey),
-                accountName: Text(
-                  "Rasel Hossain",
-                  style: TextStyle(color: Colors.amberAccent),
-                ),
-                accountEmail: Text(
-                  "rh023896@gmail.com",
-                  style: TextStyle(color: Colors.amberAccent),
-                ),
-                currentAccountPicture: Image.network(
-                  'https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg',
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text("Home"),
-              leading: Icon(Icons.home),
-              onTap: () {
-                mySnackBar('Expense Category', context);
-              },
-            ),
-            ListTile(
-              title: Text("Expense Category"),
-              leading: Icon(Icons.category),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text("Expense"),
-              leading: Icon(Icons.exposure_neg_1_sharp),
-              onTap: () {},
-            ),
+            Home(),
+            Settings(),
+            Sos(),
+            SearchFragement(),
+            User(),
+            Email(),
+            Contact(),
           ],
         ),
-      ),
-      endDrawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-              child: UserAccountsDrawerHeader(
-                decoration: BoxDecoration(color: Colors.grey),
-                accountName: Text(
-                  "Rasel Hossain",
-                  style: TextStyle(color: Colors.amberAccent),
-                ),
-                accountEmail: Text(
-                  "rh023896@gmail.com",
-                  style: TextStyle(color: Colors.amberAccent),
-                ),
-                currentAccountPicture: Image.network(
-                  'https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg',
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text("Home"),
-              leading: Icon(Icons.home),
-              onTap: () {
-                mySnackBar('Expense Category', context);
-              },
-            ),
-            ListTile(
-              title: Text("Expense Category"),
-              leading: Icon(Icons.category),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text("Expense"),
-              leading: Icon(Icons.exposure_neg_1_sharp),
-              onTap: () {},
-            ),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                child: Image.network(
-                  'https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg',
-                ),
-                alignment: Alignment.center,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.amberAccent,
-                  border: Border.all(color: Colors.blue, width: 2),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                child: Image.network(
-                  'https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
-                ),
-                alignment: Alignment.center,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.amberAccent,
-                  border: Border.all(color: Colors.blue, width: 2),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                child: Image.network(
-                  'https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg',
-                ),
-                alignment: Alignment.center,
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.amberAccent,
-                  border: Border.all(color: Colors.blue, width: 2),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextButton(
-                onPressed: () {
-                  alertDialog(context);
-                },
-                child: Text('Click me'),
-                style: buttonStyle,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  mySnackBar('Elevated Button', context);
-                },
-                child: Text('Elevated Button'),
-              ),
-            ],
-          ),
-
-          Row(
-            children: [
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 2,
-                    childAspectRatio: 1.2,
-                  ),
-                  itemCount: myListItem.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        mySnackBar(myListItem[index]['name'], context);
-                      },
-                      onDoubleTap: () {
-                        mySnackBar(
-                          myListItem[index]['age'].toString(),
-                          context,
-                        );
-                      },
-                      onLongPress: () {
-                        mySnackBar(
-                          myListItem[index]['roll'].toString(),
-                          context,
-                        );
-                      },
-
-                      child: Container(
-                        margin: EdgeInsets.all(10),
-                        width: double.infinity,
-                        height: 150,
-                        child: Image.network(
-                          myListItem[index]['img']!,
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "First Name",
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Last Name",
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Email",
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextButton(
-              onPressed: () {},
-              style: buttonStyle,
-              child: Text("Submit"),
-            ),
-          ),
-        ],
       ),
     );
   }
 }
+
+
+//  appBar: AppBar(
+//         title: Text("Flutter App"),
+//         backgroundColor: Colors.greenAccent,
+//         titleSpacing: 10,
+//         toolbarHeight: 60,
+//         toolbarOpacity: 0.6,
+//         elevation: 40,
+//       ),
