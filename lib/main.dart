d2d7c819cec509c1400e6a58ce5c13f4e880953a
bdrasel <rh88248@gmail.com>
2025-11-25 11:54:56 +0600

    flutter card design

--- lib/main.dart
+++ lib/main.dart
@@ -24,80 +24,25 @@ class HomeActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home"), backgroundColor: Colors.greenAccent),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ActivityPage("Activity Route Page"),
-                ),
-              );
-            },
-            child: Text("Activity Page"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ActivityPage2()),
-              );
-            },
-            child: Text("Activity Page2"),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ActivityPage extends StatelessWidget {
-  String msg;
-  ActivityPage(this.msg, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(msg), backgroundColor: Colors.greenAccent),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomeActivity()),
-              );
-            },
-            child: Text("Home"),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ActivityPage2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Activity Page 2"),
-        backgroundColor: Colors.greenAccent,
-      ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => ActivityPage("Coming from ActivityPage2"),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          elevation: 25,
+          shadowColor: Color.fromARGB(255, 5, 165, 27),
+          color: Color.fromARGB(255, 5, 165, 27),
+          child: SizedBox(
+            width: 200,
+            height: 150,
+
+            child: Center(
+              child: Text(
+                "This is a card",
+                style: TextStyle(color: Colors.white),
               ),
-            );
-          },
-          child: Text("Go Activity Page"),
+            ),
+          ),
         ),
       ),
     );
